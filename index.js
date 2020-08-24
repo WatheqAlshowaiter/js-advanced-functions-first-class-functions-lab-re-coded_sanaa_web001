807018c86c4d60f598424cac4233ba8a28db42aa
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-24 15:17:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 const returnFirstTwoDrivers = function (arrayOfDrivers) {
   return arrayOfDrivers.slice(0, 2);
 };
+
+const returnLastTwoDrivers = function (arrayOfDrivers) {
+  return arrayOfDrivers.slice(-2);
+};
